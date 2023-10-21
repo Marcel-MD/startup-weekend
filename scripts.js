88aa3e4aa520cf7970d6f7d7334f430e530ef5f6
AlexandruUmanet <alexandru.umanet@gmail.com>
2023-10-21 20:05:37 +0300

    - added slides

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
-// JavaScript for the slider
-const slider = document.querySelector(".slider");
-const slides = document.querySelectorAll(".slide");
-const slideWidth = slides[0].clientWidth;
-let currentIndex = 0;
+// You can also add navigation buttons or indicators for user control1
+const images = document.querySelectorAll(".main-banner img");
+let currentImageIndex = 0;
 
-function slideTo(index) {
-    if (index < 0) index = slides.length - 1;
-    if (index >= slides.length) index = 0;
-    currentIndex = index;
-    slider.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+function toggleImages() {
+    const currentImage = images[currentImageIndex];
+    const nextImageIndex = (currentImageIndex + 1) % images.length;
+    const nextImage = images[nextImageIndex];
+
+    currentImage.style.opacity = 0;
+    nextImage.style.opacity = 1;
+
+    currentImageIndex = nextImageIndex;
+
+    setTimeout(toggleImages, 2000); // Change images every 2 seconds (adjust as needed)
 }
 
-setInterval(() => {
-    slideTo(currentIndex + 1);
-}, 3000); // Auto slide every 3 seconds
-
-// You can also add navigation buttons or indicators for user control
+toggleImages(); // Start the loop
